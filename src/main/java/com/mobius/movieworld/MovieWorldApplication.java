ca3f4297c8770d9f0fa87c97d2294dd3d61135f7
chenxushan <chenxushan58@gmail.com>
2021-08-13 00:22:36 +0800

    start create

---
+++ src/main/java/com/mobius/movieworld/MovieWorldApplication.java
@@ -0,0 +1,13 @@
+package com.mobius.movieworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mobius/movieworld/MovieWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobius.movieworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
