6efac8bc7db489c8485c37bc81113c11ea5cdae8
Sunniberg <344926445@qq.com>
2021-09-27 19:01:52 +0800

    github.dev

--- knowledge/src/main/java/com/mobius/codewars/DuplicateEncoder.java
+++ knowledge/src/main/java/com/mobius/codewars/DuplicateEncoder.java
@@ -8,7 +8,6 @@ package com.mobius.codewars;
 //        "Success"  =>  ")())())"
 //        "(( @"     =>  "))(("
 //        Notes
-//
 //        Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
 
 import java.util.*;
